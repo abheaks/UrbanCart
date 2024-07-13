49e7bfcc37417abf5f86ec82d02a2fc08bf56870
Abhijith S <abheaks.007@gmail.com>
2024-07-13 18:11:32 +0530

    Initial commit

---
+++ src/main/java/com/projects/urbancart/UrbanCartApplication.java
@@ -0,0 +1,13 @@
+package com.projects.urbancart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrbanCartApplication.class, args);
+    }
+
+}
